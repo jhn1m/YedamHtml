e141d4da2d1e191b9462718b670f2a2e75b2665a
jhn1m <qqoxmaos@gmail.com>
2022-01-05 16:19:59 +0900

    16:19

--- helloWorld/src/main/webapp/javascript/function5.js
+++ helloWorld/src/main/webapp/javascript/function5.js
@@ -1,1 +1,35 @@
 // function5.js
+
+"use strict"
+
+var result = 100
+
+function sum(first, last) {
+  var result = first + last
+  console.log(result)
+}
+
+sum(10, 20)
+
+console.log(result)
+
+var result = 200
+console.log(result)
+
+// let result = 300
+
+let summay = 100
+console.log(summay)
+
+function block1() {
+  let summay = 200
+  console.log(summay)
+}
+
+console.log(summay)
+
+for (let i = 0; i < 5; i++) {
+  console.log(i)
+}
+console.log("-----")
+console.log(i)
