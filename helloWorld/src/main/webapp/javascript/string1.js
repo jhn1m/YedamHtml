bebea044e6853bd679e7540b6c56d6364232ab38
jhn1m <qqoxmaos@gmail.com>
2022-01-05 17:47:06 +0900

    17:47

--- helloWorld/src/main/webapp/javascript/string1.js
+++ helloWorld/src/main/webapp/javascript/string1.js
@@ -2,11 +2,16 @@
 
 "use strict"
 
+var val = "hello"
+val = new String("Hello")
+console.log(typeof val)
+
 var h3Tag = document.querySelector("h3")
 var text = h3Tag.innerHTML
 var str = text.substring(2, 10)
-text = text.substring(2, 6)
+str = text.slice(1, 5)
+
 var elem = document.getElementById("show")
 // elem.innerHTML = "<h3>Hello</h3>"
-elem.innerText = text
+elem.innerText = str
 console.log(elem)
