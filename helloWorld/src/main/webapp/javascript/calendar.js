4d831c014004c1a7ffb79e4d4efc5552bb74cf16
jhn1m <qqoxmaos@gmail.com>
2022-01-05 15:59:50 +0900

    15:39

--- helloWorld/src/main/webapp/javascript/calendar.js
+++ helloWorld/src/main/webapp/javascript/calendar.js
@@ -16,74 +16,75 @@
 // console.log(otherDay.getDay())
 "use strict"
 
-//#region 달력 틀
-var year = 2022
-var month = 5
-var today = new Date(year, month - 1, 1)
-var lastDay = new Date(year, month, 0) // 월의 마지막 날
-var firstDayOfMonth = today.getDay()
-var lastDateOfMonth = lastDay.getDate()
-var days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"]
-
-document.write(`<h1>${today.getFullYear()}년 ${today.getMonth() + 1}월</h1>`)
-for (var day of days) {
-  document.write(`<span>${day}</span>`)
-}
-document.write("<br>")
-
-for (var n = 0; n < today.getDay(); n++) {
-  document.write(`<span class = "calblank">&nbsp</span>`)
-}
-for (var i = 1; i <= lastDay.getDate(); i++) {
-  if (i == today.getDate()) {
-    document.write(`<span style = font-weight:bold;> ${i} </span>`)
-    if ((i + today.getDay()) % 7 == 0) {
-      document.write("<br>")
-    }
-    continue
-  }
-  document.write(`<span>${i}</span>`)
-  if ((i + today.getDay()) % 7 == 0) {
-    document.write("<br>")
-  }
-}
-document.write(`</div>`)
-//#endregion
-
-// #region 연간 달력 출력
-
-for (var i = 0; i < 12; i++) {
+function makeCal(year, month) {
+  //#region 달력 틀
   document.write(`<div id="calendar">`)
-  var tod = new Date(2022, i, 1)
-  var lad = new Date(2022, i + 1, 0)
-  var days = ["일", "월", "화", "수", "목", "금", "토"]
-  document.write(`<h1>${tod.getFullYear()}년 ${tod.getMonth() + 1}월</h1>`)
+  var today = new Date(year, month - 1, 1)
+  var lastDay = new Date(year, month, 0) // 월의 마지막 날
+  var firstDayOfMonth = today.getDay()
+  var lastDateOfMonth = lastDay.getDate()
+  var days = ["Sun", "Mon", "Tue", "Wed", "Thr", "Fri", "Sat"]
 
-  document.write(`<span class = "calheader">일</span>`)
-  document.write(`<span class = "calheader">월</span>`)
-  document.write(`<span class = "calheader">화</span>`)
-  document.write(`<span class = "calheader">수</span>`)
-  document.write(`<span class = "calheader">목</span>`)
-  document.write(`<span class = "calheader">금</span>`)
-  document.write(`<span class = "calheader">토</span>`)
+  document.write(`<h1>${today.getFullYear()}년 ${today.getMonth() + 1}월</h1>`)
+  for (var day of days) {
+    document.write(`<span class = "calheader">${day}</span>`)
+  }
   document.write("<br>")
-  for (var n = 0; n < tod.getDay(); n++) {
+
+  for (var n = 0; n < firstDayOfMonth; n++) {
     document.write(`<span class = "calblank">&nbsp</span>`)
   }
-  for (var k = 1; k <= lad.getDate(); k++) {
-    if (k == tod.getDate()) {
-      document.write(`<span style = font-weight:bold;> ${k} </span>`)
-      if ((k + tod.getDay()) % 7 == 0) {
-        document.write("<br>")
-      }
-      continue
+  for (var i = 1; i <= lastDateOfMonth; i++) {
+    if ((i + firstDayOfMonth) % 7 == 1) {
+      document.write(`<span class = "sunday"> ${i} </span>`)
+    } else if ((i + firstDayOfMonth) % 7 == 0) {
+      document.write(`<span class = "saturday"> ${i} </span>`)
+    } else {
+      document.write("<span>" + i + "</span>")
     }
-    document.write(`<span>${k}</span>`)
-    if ((k + tod.getDay()) % 7 == 0) {
+    if ((i + today.getDay()) % 7 == 0) {
       document.write("<br>")
     }
   }
   document.write(`</div>`)
+  //#endregion
 }
+function makeYearCal(year, month) {
+  // #region 연간 달력 출력
 
-//#endregion
+  for (var i = 0; i < 12; i++) {
+    document.write(`<div id="calendar">`)
+    var tod = new Date(2022, i, 1)
+    var lad = new Date(2022, i + 1, 0)
+    var days = ["일", "월", "화", "수", "목", "금", "토"]
+    document.write(`<h1>${tod.getFullYear()}년 ${tod.getMonth() + 1}월</h1>`)
+
+    document.write(`<span class = "calheader">일</span>`)
+    document.write(`<span class = "calheader">월</span>`)
+    document.write(`<span class = "calheader">화</span>`)
+    document.write(`<span class = "calheader">수</span>`)
+    document.write(`<span class = "calheader">목</span>`)
+    document.write(`<span class = "calheader">금</span>`)
+    document.write(`<span class = "calheader">토</span>`)
+    document.write("<br>")
+    for (var n = 0; n < tod.getDay(); n++) {
+      document.write(`<span class = "calblank">&nbsp</span>`)
+    }
+    for (var k = 1; k <= lad.getDate(); k++) {
+      if (k == tod.getDate()) {
+        document.write(`<span style = font-weight:bold;> ${k} </span>`)
+        if ((k + tod.getDay()) % 7 == 0) {
+          document.write("<br>")
+        }
+        continue
+      }
+      document.write(`<span>${k}</span>`)
+      if ((k + tod.getDay()) % 7 == 0) {
+        document.write("<br>")
+      }
+    }
+    document.write(`</div>`)
+  }
+
+  //#endregion
+}
--- helloWorld/src/main/webapp/javascript/function4.js
+++ helloWorld/src/main/webapp/javascript/function4.js
@@ -5,41 +5,59 @@ var students = [
     name: "홍길동",
     score: 60,
     birth: "1999-12-30",
+    phone: "010-1111-2222",
+    gender: "남",
   },
   {
     name: "김가삿",
     score: 70,
     birth: "1979-12-30",
+    phone: "010-1111-2222",
+    gender: "여",
   },
   {
     name: "고운오",
     score: 90,
     birth: "1999-12-30",
+    phone: "010-1111-2222",
+    gender: "여",
   },
   {
     name: "케이크",
     score: 10,
     birth: "2009-12-30",
+    phone: "010-1111-2222",
+    gender: "여",
   },
   {
     name: "갹으동",
     score: 62,
     birth: "1992-12-30",
+    phone: "010-1111-2222",
+    gender: "남",
   },
 ]
 
 function makeTable(ary) {
   var str = ""
   str += `<table border='1'>`
-  str += "<thead><tr><th>이름</th><th>점수</th><th>생년월일</th></tr></thead>"
+  str += `<thead><tr><th>이름</th><th>점수</th><th>생년월일</th><th>연락처</th><th>성별</th></tr></thead>`
   str += `<tbody>`
   for (var student of ary) {
-    str += "<tr>"
-    str += `<td>${student.name}</td>`
-    str += `<td>${student.score}</td>`
-    str += `<td>${student.birth}</td>`
-    str += "</tr>"
+    str += `<tr>`
+    for (var field in student) {
+      str += `<td>${student[field]}</td>`
+    }
+    // str += `<td>${student.name}</td>`
+    // str += `<td>${student.score}</td>`
+    // str += `<td>${student.birth}</td>`
+    // str += `<td>${student.phone}</td>`
+    str += `</tr>`
   }
   str += "</tbody>"
   str += "</table>"
+  return str
 }
+
+document.write(makeTable(students))
+makeCal(2022, 5)
