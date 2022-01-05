06d0db74df39346a9d1602e4f72fcfb369b05981
jhn1m <qqoxmaos@gmail.com>
2022-01-05 09:49:57 +0900

    09:49

---
+++ helloWorld/src/main/webapp/javascript/sample1.js
@@ -0,0 +1,19 @@
+// sample1.js
+"use strict"
+var memNum = window.prompt("입장객은 몇명?")
+var colNum = window.prompt("한 줄에 앉을 사람?")
+
+// 56명, 5명씩
+
+document.write(`입장객은 ${memNum}명 입니다.`)
+document.write(`한 줄에 ${colNum}명씩 앉아주세요.`)
+document.write(`<br>`)
+document.write("<h2>좌석표시</h2>")
+for (var i = 1; i <= memNum; i++) {
+  for (var j = 1; j <= colNum; j++) {
+    document.write(`<p>${i}열 - ${j}번</p>`)
+    if (j % colNum == 0) {
+      document.write(`<br>`)
+    }
+  }
+}
