180239c86f99fe547a6914711879b88adbf9e33d
jhn1m <qqoxmaos@gmail.com>
2022-01-07 16:33:03 +0900

    16:33

--- helloWorld/src/main/webapp/javascript/dom5.js
+++ helloWorld/src/main/webapp/javascript/dom5.js
@@ -20,34 +20,56 @@ const data = [
     email: "Kim@email.com",
   },
 ]
+
 function makeHead() {
+  const field = ["Name", "Phone", "Addr", "Email"]
   let thd = document.createElement("thead")
   let tr = document.createElement("tr")
-  let th = document.createElement("th")
-  let text = document.createTextNode("이름")
-  th.appendChild(text)
-  tr.appendChild(th)
+
+  // 체크박스
+  let td = document.createElement("td")
+  let checkbox = document.createElement("input")
+  checkbox.setAttribute("type", "checkbox")
+  td.appendChild(checkbox)
+  tr.appendChild(td)
+
+  field.forEach(function (field) {
+    let th = document.createElement("th")
+    let text = document.createTextNode(field)
+    th.appendChild(text)
+    tr.appendChild(th)
+  })
   thd.appendChild(tr)
 
   return thd
 }
 
+function makeBody() {
+  let tbd = document.createElement("tbody")
+  data.forEach(function (obj) {
+    tbd.appendChild(makeTr(obj))
+  })
+  return tbd
+}
+
 // tr 생성
-function makeTr() {
+function makeTr(obj) {
   let tr = document.createElement("tr")
+
+  // 체크박스
+  let td = document.createElement("td")
+  let checkbox = document.createElement("input")
+  checkbox.setAttribute("type", "checkbox")
+  td.appendChild(checkbox)
+  tr.appendChild(td)
+
   for (let field in obj) {
     let td = document.createElement("td")
     let text = document.createTextNode(obj[field])
     td.appendChild(text)
     tr.appendChild(td)
   }
-  tbd.appendChild(tr)
-}
-
-function makeBody() {
-  let tbd = document.createElement("tbody")
-  data.forEach(function (obj) {})
-  return tbd
+  return tr
 }
 
 // 즉시 실행 함수 > 괄호 안에 넣으면 함수 이름이 없어도 실행됨
@@ -58,4 +80,40 @@ function makeBody() {
   tbl.appendChild(makeBody())
   document.getElementById("list").appendChild(tbl)
 })()
-let parent = document.getElementById("show")
+
+// 등록버튼 이벤트
+let addBtn = document.querySelector("#btn > button")
+addBtn.addEventListener("click", addCallback)
+
+function addCallback() {
+  let name = document.querySelector("input[name = 'name']").value
+  let phone = document.querySelector("input[name = 'phone']").value
+  let addr = document.querySelector("input[name = 'addr']").value
+  let email = document.querySelector("input[name = 'email']").value
+  if (name == "" || phone == "" || addr == "" || email == "") {
+    window.alert("필수값을 입력하세요 !")
+    return
+  }
+
+  let obj = {
+    name: name,
+    phone: phone,
+    addr: addr,
+    email: email,
+  }
+  let tr = makeTr(obj)
+  document.querySelector("#list > table > tbody").appendChild(tr)
+}
+
+// 삭제버튼 이벤트 등록
+let delBtn = document.querySelector("#btn > button:nth-child(2)")
+delBtn.addEventListener("click", delCallback)
+
+function delCallback() {
+  let chks = document.querySelectorAll("tbody input[type = 'checkbox']")
+    for (let i = 0; i < chks.length; i++){
+        if (chks[i].checked == true) {
+            
+        }
+    }
+}
